5ac884a567c661d54331a4d79b93909c750592d7
Anastasiyu <asy.safiulina@mail.ru>
2022-10-19 16:27:27 +0300

    Initial commit

---
+++ src/test/java/com/example/calculatorhomeworktests/CalculatorHomeWorkTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculatorhomeworktests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorHomeWorkTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
